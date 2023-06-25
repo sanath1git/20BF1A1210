3ae37d515e736cd34fcb0a4caa84140d087a323c
Sanath Kumar <sanath2003kumar@gmail.com>
2023-06-25 14:50:08 +0530

    Changed task1

--- task1.py
+++ task1.py
@@ -9,18 +9,21 @@ app = Flask(__name__)
 
 @app.route('/trains',methods=['GET'])
 def GET_TRAINS():
+    res = {}
     token = os.environ['ACCESS_TOKEN']
-    response = requests.get('http://104.211.219.98/train/trains', headers={'Authorization': 'access_token {token}'})
-
+    response = requests.get('http://104.211.219.98/train/trains', headers={'Authorization': 'access_token {token}'}).json()
     current_time = datetime.now()
 
+    min_departure_time = current_time + timedelta(minutes=30)
 
-    return ""
+    filtered_trains = [
+        train for train in response['trains']
+        if datetime.strptime(response['departure_time'], '%H:%M:%S') + timedelta(minutes=response["delay"]) > min_departure_time.time() > min_departure_time
+    ]
+
+    filtered_trains = sorted(filtered_trains,key=lambda train: (-train['availability'], train['fare']))
+
+    return filtered_trains
  
-
-
-
 if __name__ == '__main__':
- 
-    
     app.run()
